1317614bc74b5f3d792313706e8846e3b11e90e1
SharrrkNado <tjark12@gmail.com>
2026-05-19 19:07:20 +0200

    project content

--- src/content.config.ts
+++ src/content.config.ts
@@ -17,4 +17,32 @@ const blog = defineCollection({
 		}),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/project', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: z.optional(image()),
+		}),
+});
+
+
+const tutorial = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    id: z.string(),
+    description: z.string(),
+    category: z.string(),
+    prerequisites: z.array(z.string()).default([]),
+    tier: z.number(),
+  }),
+});
+
+export const collections = { blog, tutorial, project };
